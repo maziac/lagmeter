e88cd02eb4d19ab3b34d974e5ea50fa20e086589
maziac <thomas.busse@gmx.net>
2019-12-01 14:29:50 +0100

    Modified HidUniversal to be able to change pollingInterval.

--- Arduino/LagMeter/LagMeter.ino
+++ Arduino/LagMeter/LagMeter.ino
@@ -59,8 +59,7 @@ const int KEY_MEASURE_SVGA = LCD_KEY_UP;
 const int KEY_MEASURE_SVGA_TO_PHOTO = LCD_KEY_RIGHT;
 const int KEY_MEASURE_MIN_TIME = LCD_KEY_LEFT;
 // usblag:
-const int KEY_USBLAG_MEASURE_1MS = LCD_KEY_DOWN;
-const int KEY_USBLAG_MEASURE_8MS = LCD_KEY_UP;
+const int KEY_USBLAG_MEASURE     = LCD_KEY_DOWN;
 const int KEY_USBLAG_TEST_BUTTON = LCD_KEY_SELECT;
 
 
@@ -110,6 +109,13 @@ const int USBLAG_CYCLES = 100;
 #define BUTTON_PIN 8
 
 
+// Time to show the title of each test.
+#define TITLE_TIME  1500    // in ms
+
+// Count of cycles to measure the input lag.
+const int COUNT_CYCLES = 100;
+
+
 // Prints a different pattern for button press or release.
 void printJoystickButtonChanged() {
   static bool lastButtonPattern = false;
@@ -125,80 +131,6 @@ void printJoystickButtonChanged() {
 }
 
         
-// Prints the measured time for usblag.
-// @param measuredTime The measured time in ms.
-int total;
-float usblagMin;
-float usblagMax;
-float usblagAvg;
-//bool usbEventToggle = false;
-void printUsbLag(float measuredTime) {
-    if(total < 0) {
-      lcd.setCursor(0,1);
-      // Print req. poll interval
-      lcd.print(requestedPollInterval);
-      lcd.print(F("ms "));
-      // Print something in 2nd line. Can be used as a test that game controller reacts.
-      //usbEventToggle = !usbEventToggle;
-      if(joystickButtonPressed)
-        lcd.print(F("####----####"));
-      else
-        lcd.print(F("----####----"));
-      return;
-    }
-    if(total > USBLAG_CYCLES )
-      return;
-
-    // Init if first measurement 
-    if(total <= 1) {
-      usblagMin = 1000000.0;
-      usblagMax = 0.0;
-      usblagAvg = 0.0;
-    }
-    // Print cycle
-    lcd.setCursor(0,0);
-    lcd.print(total);
-    lcd.print(F("/"));
-    lcd.print(USBLAG_CYCLES);
-    lcd.print(F(": "));
-    // Print time
-    lcd.print(measuredTime);
-    lcd.print(F("ms     "));
-    
-    // Calculate max/min.
-    if(measuredTime > usblagMax)
-      usblagMax = measuredTime;
-    if(measuredTime < usblagMin)
-      usblagMin = measuredTime;
-
-    // Print min/max result
-    lcd.setCursor(0,1);
-    if(usblagMin != usblagMax) {
-      lcd.print(usblagMin);
-      lcd.print(F("-"));
-    }
-    lcd.print(usblagMax);
-    lcd.print(F("ms           "));
-
-    // Calculate average
-    usblagAvg += measuredTime;
-
-    // Print average if last value
-    if(total == USBLAG_CYCLES) {
-      // Print average:
-      usblagAvg /= USBLAG_CYCLES;
-      lcd.setCursor(0,0);
-      lcd.print(F("Avg: "));
-      lcd.print(usblagAvg);
-      lcd.print(F("ms         "));
-      // Make sure button is off
-      digitalWrite(BUTTON_PIN, false);
-      // End measurement
-      total++;
-    }
-}
-
-
 
 // SETUP
 void setup() {
@@ -327,94 +259,127 @@ void usblagTestButton() {
 }
 
 
+// Measures the usb lag. I.e. the time from button press to received USB reaction.
+void usblagMeasure() {
+  // Show test title
+  lcd.clear();
+  lcd.print(F("Test: USB lag"));
+  waitMs(TITLE_TIME); if(isAbort()) return;
+
+  // Initialize
+  digitalWrite(BUTTON_PIN, LOW);
+
+  // Calibrate, i.e. wait a small moment 
+  lcd.clear();
+  lcd.print(F("Calibr. Don't"));
+  lcd.setCursor(0, 1);
+  lcd.print(F("touch joystick."));
+
+  for(int i=0; i<1000; i++) {
+    Usb.Task();
+    waitMs(2); if(isAbort()) return;
+  }
+  setHidIdleValues();  
+
+  lcd.clear();
+  for(int i=1; i<=COUNT_CYCLES; i++) {
+    // Print
+    lcd.setCursor(0,0);
+    Usb.Task();
+    lcd.print(i);
+    Usb.Task();
+    lcd.print(F("/"));
+    Usb.Task();
+    lcd.print(COUNT_CYCLES);
+    Usb.Task();
+    lcd.print(F(": "));
+    Usb.Task();
+
+    // Wait a random time to make sure we really get different results.
+    uint8_t waitRnd = random(70, 150);
+    for(uint8_t i=0; i<waitRnd; i++) {
+      delay(1);
+      if(isAbort()) return;
+      Usb.Task();
+    }
+  
+    // Wait until no button press
+    long startTime = millis();
+    while(joystickButtonPressed) {
+      //waitMs(1); 
+      if(isAbort()) return;   
+      // Check if too long
+      long stopTime = millis();
+      long diffTime = stopTime - startTime;     
+      if(diffTime > 1000) {
+        // More than a second
+        Error(F("Error:"), F("No response!a"));
+        return;
+      }
+      Usb.Task();
+    }
+    
+    for(uint8_t i=0; i<100; i++)
+      Usb.Task();
+  
+    // Start meassuring
+    startTime = millis();
+    // "Press" button
+    Usb.Task();
+    digitalWrite(BUTTON_PIN, HIGH);
+
+    // Wait until button press
+    long diffTime = 0;
+    Usb.Task();
+    while(!joystickButtonPressed) {
+      if(isAbort()) return;
+      // Stop measuring
+      long stopTime = millis();
+      diffTime = stopTime - startTime;
+      // Check if too long
+      if(diffTime > 1000) {
+        // More than a second
+        Error(F("Error:"), F("No response!b"));
+        return;
+      }
+      if(diffTime < 0) {
+        // Should not happen
+        Error(F("Error:"), F("Internal problem!"));
+        return;
+      }
+      Usb.Task();
+    }
+
+    // Output result: 
+    lcd.print(diffTime);
+    Usb.Task();
+    lcd.print(F("ms     "));
+    Usb.Task();
+
+    // "Release" button
+    digitalWrite(BUTTON_PIN, LOW);
+  }
+}
+
+
 // Checks for keypresses for usblag mode.
 void handleUsblag() {
-  static bool prevJoystickButtonPressed = false;
-  
-  // Check to print the menu
-  if(abortAll) {
-    digitalWrite(BUTTON_PIN, false);
-    abortAll = false;
-    total = -1; // Show game controller action
-    printUsblagMenu();
-  }
-
   // Prints patterns if joystick button has changed
   printJoystickButtonChanged();
 
   // Handle user input
   int key = getLcdKey();
-  if(key != LCD_KEY_NONE) {
-    // Check if we should abort the current measurement
-    if(total > 0) {
-      abortAll = true;
-      return;
-    }
-  }
   switch(key) {
     case KEY_USBLAG_TEST_BUTTON:
       // ON/OFF of the button and showing the result. I.e. a quick test to see if the connection is OK.
       usblagTestButton();
-      //return;
       break;
-    case KEY_USBLAG_MEASURE_1MS:
+            
+    case KEY_USBLAG_MEASURE:
       // Start testing usb device measurement
-      button = false;
-      total = 0;
-      digitalWrite(BUTTON_PIN, button);
-      time = 0;
-      Serial.print(F("Launching test"));
-//      nextchange = micros() + random(50, 70)*1000 + random(0, 250)*4;
-      // Use USB polling interval of 1ms.
-     // Hid.overrideInterval = 1;
-     // xbox.overrideInterval = 1;
-      // Inform user
-      lcd.clear();
-      lcd.print(F("1ms poll..."));
-
-      // Wait for potential lag time
-      waitMs(1000); 
-      lcd.clear();
-      if(isAbort()) return;
-      break;
-      
-    case KEY_USBLAG_MEASURE_8MS:
-      // Start testing usb device measurement
-      button = false;
-      total = 0;
-      digitalWrite(BUTTON_PIN, button);
-      time = 0;
-      Serial.print(F("Launching test"));
-//      nextchange = micros() + random(50, 70)*1000 + random(0, 250)*4;
-      // Use USB polling interval of 8ms.
-     // Hid.overrideInterval = 8;
-     // xbox.overrideInterval = 8;
-      // Inform user
-      lcd.clear();
-      lcd.print(F("8ms poll..."));
-      // Wait for potential lag time
-      waitMs(1000); 
-      lcd.clear();
-      if(isAbort()) return;
+      usblagMeasure();
       break;
   }
-
-#if 0
-  // Handle time
-  unsigned long current_time = micros();
-  if (current_time >= nextchange && total >= 0 && total < USBLAG_CYCLES) {
-    if (time != 0) {
-      Error(F("USB device:"), F("Input dropped!"));
-      Serial.println("Input was dropped!");
-      return;
-    }
-    button = !button;
-    digitalWrite(BUTTON_PIN, button);
-    time = micros();
-    total++;
-    nextchange = micros() + random(50, 70)*1000 + random(0, 250)*4;
-  }
-#endif
 }
 
 
@@ -427,7 +392,6 @@ void loop() {
     if(usbMode) {
       // Usblag mode
       printUsblagMenu();
-      total = -1; // Show game controller action
     }
     else {
       // Reset
--- Arduino/LagMeter/USBHIDJoystick.ino
+++ Arduino/LagMeter/USBHIDJoystick.ino
@@ -1,5 +1,6 @@
 #include <usbhid.h>
-#include <hiduniversal.h>
+//#include <hiduniversal.h>
+#include "src/usb/modifiedhiduniversal.h"
 #include <usbhub.h>
 //#include <MemoryUsage.h>
 
@@ -24,15 +25,18 @@ public:
     for(uint8_t i=0; i<MAX_VALUES; i++) {
       idleValues[i] = lastReportedValues[i];
     }
+    joystickButtonPressed = false;
   }
 
   void JoystickReportParser::Parse(USBHID* hid, bool is_rpt_id, uint8_t len, uint8_t* buf) {
+    /*
     // Print
     for(uint8_t i=0; i<len; i++) {
       SerialPrintHex<uint8_t>(buf[i]); 
       Serial.print(F(", "));
     }
     Serial.println();
+    */
     
     // Check if changed
     uint8_t count = min(MAX_VALUES, len);
@@ -45,50 +49,46 @@ public:
       }
     }
 
-    // Return if no change
-    if(match)
-      return;
-
     // Suppress for some time (1s)
     unsigned long time = millis();
-    Serial.print(time);
-    Serial.print(F(", "));
-    Serial.print(startTime);
+    //Serial.print(time);
+    //Serial.print(F(", "));
+    //Serial.print(startTime);
     if(startTime == 0)
       startTime = time;
     if(time > startTime+1000)
       joystickButtonChanged = true;
-    Serial.print(F(", "));
-    Serial.println(joystickButtonChanged);
+    //Serial.print(F(", "));
+    //Serial.println(joystickButtonChanged);
     
     // Values changed. Check if "button press"
     for(uint8_t i=0; i<count; i++) {
       if(idleValues[i] != lastReportedValues[i]) {
         // Some button was pressed.
         joystickButtonPressed = true;
-        Serial.println("joystickButtonPressed = true");
+        //Serial.println("joystickButtonPressed = true");
         return;
       }
     }
 
     // Idle
     joystickButtonPressed = false;
-    Serial.println("joystickButtonPressed = false");
-   }
+    //Serial.println("joystickButtonPressed = false");
+  }
 };
 
 JoystickReportParser HidJoyParser;
 
 
 
-class UsbHidJoystick : public HIDUniversal {
+class UsbHidJoystick : public ModifiedHIDUniversal {
 public:
   uint8_t pollIntervalCopy; // The real variable is private.
   
-  UsbHidJoystick(USB* p) : HIDUniversal (p), pollIntervalCopy(0) {};
+  UsbHidJoystick(USB* p) : ModifiedHIDUniversal (p), pollIntervalCopy(0) {};
   
   virtual uint8_t Init (uint8_t parent, uint8_t port, bool lowspeed) {
-    uint8_t res = HIDUniversal::Init(parent, port, lowspeed);
+    uint8_t res = ModifiedHIDUniversal::Init(parent, port, lowspeed);
     Serial.println("UsbHidJoystick::Init done.");
     Serial.print("Poll interval: ");
     Serial.print(pollIntervalCopy);
@@ -99,7 +99,7 @@ public:
   }
   
   virtual uint8_t Release () {
-    uint8_t res = HIDUniversal::Release();
+    uint8_t res = ModifiedHIDUniversal::Release();
     Serial.println("UsbHidJoystick::Release done.");
     usbMode = false;
     return res;
@@ -107,7 +107,7 @@ public:
 
 
   virtual void EndpointXtract(uint8_t conf, uint8_t iface, uint8_t alt, uint8_t proto, const USB_ENDPOINT_DESCRIPTOR* ep) {
-    HIDUniversal::EndpointXtract(conf, iface, alt, proto, ep);
+    ModifiedHIDUniversal::EndpointXtract(conf, iface, alt, proto, ep);
     if(pollIntervalCopy == 0)
       pollIntervalCopy = ep->bInterval; // Use the first found interval
     Serial.print("UsbHidJoystick::EndpointXtract done. Interface ");
@@ -131,4 +131,11 @@ void USBHIDInit() {
     Serial.println("SetReportParser problem.");
     Error(F("Error:"), F("SetReportParser!!!"));
   }
+}
+
+
+
+// Sets the idle values for the joystick. I.e. no button pressed.
+void setHidIdleValues() {
+  HidJoyParser.setIdleValues();
 }
